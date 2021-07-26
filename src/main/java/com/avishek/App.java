f32779d04a8e1bc44cb355e90f4df0cb6e881d58
root <root@ip-172-31-15-50.us-east-2.compute.internal>
2021-07-26 17:55:57 +0000

    First commit of java standalone project

---
+++ src/main/java/com/avishek/App.java
@@ -0,0 +1,13 @@
+package com.avishek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
